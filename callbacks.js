191a37fb7d0903e862fdf27c010da745b0fb2af4
HelenaGiuffrida <helenadelbianco@hotmail.com>
2021-10-20 09:28:08 -0300

    quarto array

---
+++ callbacks.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Ju", "Leo", "Paula"]
+
+nomes.forEach(imprimiNomes)
+
+function imprimiNomes(nome){
+    console.log(nome)
+}
--- medias-for-each.js
+++ medias-for-each.js
@@ -4,7 +4,8 @@ let somaDasNotas = 0
 
 
 //callback
-notas.forEach( nota => {somaDasNotas += nota})
+notas.forEach(nota => {somaDasNotas += nota})
+//notas.forEach(function(nota){somaDasNotas += nota})
 
 let media = somaDasNotas/notas.length
 
